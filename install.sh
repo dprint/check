5693fb6de1c89e72bd95731817c4788d0b1c0a19
David Sherret <dsherret@gmail.com>
2021-09-12 13:36:25 -0400

    And another.

--- install.sh
+++ install.sh
@@ -4,5 +4,5 @@ if [ "$1" -eq "latest" ]
 then
   curl -fsSL https://dprint.dev/install.sh | sh > /dev/null 2>&1
 else
-  curl -fsSL https://dprint.dev/install.sh | sh -s $0 > /dev/null 2>&1
+  curl -fsSL https://dprint.dev/install.sh | sh -s "$1" > /dev/null 2>&1
 fi
